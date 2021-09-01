114b315b095ba0c28835f6f5587319c473545937
Bk49 <ericngyongwei@gmail.com>
2021-09-01 10:00:48 +0800

    Create Paragraph Text

---
+++ src/components/ui/common/text/paragraph/ParagraphText.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>
+        <slot />
+    </p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@font-face {
+    font-family: TitilliumWeb;
+    src: url(../../../../../assets/fonts/TitilliumWeb-Regular.ttf);
+}
+
+p {
+    font-family: TitilliumWeb;
+    font-weight: 400;
+    color: #f0f0f0;
+}
+</style>
