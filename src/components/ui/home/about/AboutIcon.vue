e37daf50b63b546b0a89e1a9279ea72a8926a4b7
Bk49 <ericngyongwei@gmail.com>
2021-09-01 10:00:59 +0800

    Create About Icon

---
+++ src/components/ui/home/about/AboutIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <img :src="require(`@/assets/icons/${file}`)" />
+</template>
+
+<script>
+export default {
+    name: "AboutIcon",
+    props: {
+        file: String,
+    },
+};
+</script>
+
+<style scoped>
+img {
+    filter: invert(99%) sepia(3%) saturate(556%) hue-rotate(192deg)
+        brightness(115%) contrast(88%);
+    vertical-align: middle;
+    width: 100%;
+}
+</style>
