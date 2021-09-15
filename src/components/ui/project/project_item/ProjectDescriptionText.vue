c761280c608013c8fdfc7544ba02fb0d56b2f07c
Bk49 <ericngyongwei@gmail.com>
2021-09-15 15:18:56 +0800

    Create ProjectDescriptionText

---
+++ src/components/ui/project/project_item/ProjectDescriptionText.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="description">
+        <span>
+            <slot> </slot>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProjectDescriptionText",
+    props: {
+        text: String,
+        link: String,
+    },
+};
+</script>
+
+<style scoped>
+@font-face {
+    font-family: TitilliumWeb;
+    src: url(../../../../assets/fonts/TitilliumWeb-Regular.ttf);
+}
+.description {
+    color: #f4f4ed;
+    display: table;
+}
+
+span {
+    font-family: TitilliumWeb;
+    font-weight: 400;
+    font-size: 3vh;
+    display: table-cell;
+    vertical-align: middle;
+}
+</style>
