542b078e23854b561ef5f9ae57a468866cd71248
Bk49 <ericngyongwei@gmail.com>
2021-09-20 21:41:43 +0800

    Add project details header components

---
+++ src/components/ui/project_details/ProjectHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1><slot /></h1>
+</template>
+
+<script>
+export default {
+    name: "ProjectHeader",
+};
+</script>
+
+<style scoped>
+@font-face {
+    font-family: HemiHead;
+    src: url(../../../assets/fonts/HemiHead.ttf);
+}
+
+h1 {
+    margin: 0;
+    font-family: HemiHead;
+    font-style: italic;
+    font-weight: bold;
+    font-size: 3.3vw;
+    color: #70d6ff;
+    text-shadow: 4px 4px 4px #050505;
+}
+</style>
---
+++ src/components/ui/project_details/ProjectSubHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1><slot /></h1>
+</template>
+
+<script>
+export default {
+    name: "ProjectSubHeader",
+};
+</script>
+
+<style scoped>
+@font-face {
+    font-family: HemiHead;
+    src: url(../../../assets/fonts/HemiHead.ttf);
+}
+
+h1 {
+    font-family: HemiHead;
+    font-style: italic;
+    font-weight: bold;
+    font-size: 3.2vw;
+    color: #f4f4ed;
+}
+</style>
